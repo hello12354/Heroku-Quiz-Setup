03ec07419ebda33508b1deb5012a6eda2686d791
singhb2020 <singh.bishneet@gmail.com>
2021-02-25 10:55:21 -0700

    quiz url edit

--- poc/quiz/urls.py
+++ poc/quiz/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.quiz, name='quiz'),
-    path('recommendations', views.recommendations, name='recommendations'),
+    path(r'quiz', views.quiz, name='quiz'),
+    path(r'recommendations', views.recommendations, name='recommendations'),
     url(r'submit', views.submit, name='submit'),
     url(r'startQuiz', views.quiz, name='startQuiz'),
     url(r'programInfo', views.programs, name='programInfo'),
